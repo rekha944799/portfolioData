ea6c59cf6d223c7f35feeb76aae686ab8017df1a
rekha944799 <rekhapatil9331@gmail.com>
2026-02-20 19:01:35 +0530

    Add files via upload

---
+++ personalData.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/project")
+def project():
+    return render_template("project.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        print(name, email, message)  # later connect DB/email
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
